95ad6dc5b29eacb319884f23efa53b2265855116
Johnkhay <johnkingsleyegeonu@gmail.com>
2023-04-15 17:30:28 +0100

    array example

---
+++ Filter out the geese/Array/app.js
@@ -0,0 +1,5 @@
+function switchItUp(number){
+    //Write your own Code!
+      let strNo = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+      return strNo[number]
+    }
